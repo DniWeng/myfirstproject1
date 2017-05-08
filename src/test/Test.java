131c206e5ee2d7634f005326cb3cf5a028874f6a
GanpiWeng <934059616@qq.com>
2017-05-08 17:25:05 +0800

    first

---
+++ src/test/Test.java
@@ -0,0 +1,10 @@
+package test;
+
+/**
+ * Created by Administrator on 2017/5/8 0008.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.print("hello");
+    }
+}
